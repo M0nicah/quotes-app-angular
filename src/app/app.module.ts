905cfee223154d4ab0934184b0c3031b976c2f8f
M0nicah <monicawughanga@gmail.com>
2022-04-06 11:56:21 +0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { QuoteCardComponent } from './quote-card/quote-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteFormComponent,
+    QuoteCardComponent
   ],
   imports: [
     BrowserModule
